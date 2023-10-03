439c83fa6b0b826c9ba2fec9f364085a3280ed92
YOUSSEFFELOMARI <elomari.youssef.ing@gmail.com>
2023-10-03 17:06:54 +0100

    fix all problems

---
+++ src/main/java/com/EnsaA/ConstructionApp/audit/AuditAwareImpl.java
@@ -0,0 +1,15 @@
+package ma.uiz.observatoire.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("AuditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.ofNullable((SecurityContextHolder.getContext().getAuthentication().getName()));
+    }
+}
