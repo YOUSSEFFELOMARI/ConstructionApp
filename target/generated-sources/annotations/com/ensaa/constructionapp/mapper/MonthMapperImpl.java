0e1734707c8be63ddecf57898e0e53bb59cab779
YOUSSEFFELOMARI <elomari.youssef.ing@gmail.com>
2024-01-22 21:33:04 +0100

    update corsFilter For full Deployement

--- target/generated-sources/annotations/com/ensaa/constructionapp/mapper/MonthMapperImpl.java
+++ target/generated-sources/annotations/com/ensaa/constructionapp/mapper/MonthMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-21T20:18:06+0100",
+    date = "2024-01-22T21:32:33+0100",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 19.0.2 (Amazon.com Inc.)"
 )
 @Component
